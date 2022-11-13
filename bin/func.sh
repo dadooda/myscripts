583d304618de73d87c6fbac6734c5f37bce03ee9
Alex Fortuna <fortunadze@gmail.com>
2022-11-13 18:19:45 +0300

    feat: `func.sh:abort()`

--- bin/func.sh
+++ bin/func.sh
@@ -4,7 +4,21 @@
 # Print Bash code.
 #
 
-echo '# Return 0 if verbose.
+echo '# Exit script with an optional message ($1) and line number ($2).
+#
+#   abort
+#   abort "Some error"
+#   abort "Some error" ${LINENO}
+abort() {
+  if [ -n "${1:-}" -a -n "${2:-}" ]; then
+    echo "Abort at line ${2}: ${1}" >&2
+  else
+    echo "Abort: ${1:-(no message)}" >&2
+  fi
+  exit 1
+}
+
+# Return 0 if verbose.
 is_verbose() {
   [ -n "${VERBOSE:-}" ]
 }'
