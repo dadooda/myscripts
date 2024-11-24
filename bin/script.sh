d1708fd9b1e2b16f8af0fd102c4989df0659be64
Alex Fortuna <fortunadze@gmail.com>
2024-11-25 02:28:33 +0300

    [skip ci] Moving on

--- bin/script.sh
+++ bin/script.sh
@@ -11,7 +11,11 @@ echo '#!/usr/bin/env bash
 '"$(strict.sh)"'
 
 if [[ $# = 0 ]]; then
+  echo >&2
+  echo "The Mighty Script"
+  echo >&2
   echo "USAGE: ${SN} <args>" >&2
   echo "EXAMPLE: ${SN} xyz 123" >&2
+  echo >&2
   exit 1
 fi'
