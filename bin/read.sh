096ba581cf9b83bede5883c38296f5ab5061a503
Alex Fortuna <fortunadze@gmail.com>
2024-12-11 01:44:13 +0300

    [skip ci] Moving on

--- bin/read.sh
+++ bin/read.sh
@@ -6,4 +6,8 @@
 
 echo 'DFL="Y"; read -ep "Run above command? [$DFL]: "
 REPLY=${REPLY:-$DFL}
-[[ ${REPLY,,} = "y" ]] || exit 0'
+[[ ${REPLY,,} = "y" ]] || exit 0
+
+local DFL="Y"; read -ep "Run above command? [$DFL]: "
+local REPLY=${REPLY:-$DFL}
+[[ ${REPLY,,} = "y" ]] || return 0'
